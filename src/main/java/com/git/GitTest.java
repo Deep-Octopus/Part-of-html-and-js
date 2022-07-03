d5fac004151ed2a9d8b5e03fa845a21f99dbddc2
max <1687723655@qq.com>
2022-07-03 14:46:42 +0800

    second commit

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -3,5 +3,6 @@ package com.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hallo git!");
+        System.out.println("hallo hit j");
     }
 }
